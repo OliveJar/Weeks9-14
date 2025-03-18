ff824e345fe9b1a0b5cb8fae5503a379dd664837
encru <encru@GIGITY4060>
2025-03-18 16:01:10 -0400

    finished clock that stops every hour

---
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KitClock : MonoBehaviour
+{
+    public float timeAnHourTakes = 5;
+
+    public float t;
+    public int hour = 0;
+
+    [SerializeField]
+    private Transform minuteHand;
+
+    [SerializeField]
+    private Transform hourHand;
+
+    public UnityEvent OnTheHour;
+
+    void Start()
+    {
+        StartCoroutine(time());
+    }
+
+    private IEnumerator time ()
+    {
+        t = 0;
+
+        while (t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+            minuteHand.Rotate(0, 0, -(360 / timeAnHourTakes) * Time.deltaTime);
+            hourHand.Rotate(0, 0, -(360 / (timeAnHourTakes * 12)) * Time.deltaTime);
+
+            yield return null;
+        }
+        OnTheHour.Invoke();
+    }
+
+}
