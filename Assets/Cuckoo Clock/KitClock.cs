4d9f26273629ae15a34e93f49db53a6cae98e27e
encru <encru@GIGITY4060>
2025-03-18 16:09:51 -0400

    you can now only start the clock when the minute hand is at 0

--- Assets/Cuckoo Clock/KitClock.cs
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -25,7 +25,7 @@ public class KitClock : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && t >= 5)
         {
             StopAllCoroutines();
             StartCoroutine(runTheClock());
