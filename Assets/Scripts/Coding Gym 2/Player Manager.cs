b4ab562cdb552508f8081d146574c7914bf3fd40
encru <encru@GIGITY4060>
2025-03-18 17:29:33 -0400

    Finished playr manager and coding gym

--- Assets/Scripts/Coding Gym 2/Player Manager.cs
+++ Assets/Scripts/Coding Gym 2/Player Manager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private bool Switched;
+    [SerializeField]
+    private PlayerMovement player1;
+    [SerializeField]
+    private PlayerMovement player2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,34 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Switched)
+        {
+            StartCoroutine(switchplayer2());
+        }
+        if (!Switched)
+        {
+            StartCoroutine(switchplayer1());
+        }
+    }
+
+
+
+    private IEnumerator switchplayer1()
+    {
+        player2.enabled = false;
+        player1.enabled = true;
+        yield return new WaitForSeconds(10);
+        Switched = true;
+
+        Debug.Log("Switched to player 1");
+    }
+    private IEnumerator switchplayer2()
+    {
+        player1.enabled = false;
+        player2.enabled = true;
+        yield return new WaitForSeconds(10);
+        Switched = false;
+
+        Debug.Log("Switched to player 2");
     }
 }
